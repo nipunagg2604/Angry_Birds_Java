4edce9b8f1f68e6367b3b28de779b70592879ffe
Ashwin Singh <138316564+Ashwin-996@users.noreply.github.com>
2024-11-26 19:38:39 +0530

    Add files via upload

---
+++ assets/Level_tmx_files/soil.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="soil" tilewidth="32" tileheight="32" tilecount="220" columns="20">
+ <image source="../../../Downloads/soil.png" width="666" height="375"/>
+</tileset>
